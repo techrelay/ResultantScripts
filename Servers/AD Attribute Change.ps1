b64ace8da02ceb98a376fddb4b7b7b4c637176aa
Zippy <zippy@nbennett.xyz>
2022-03-22 16:47:16 -0400

    Delta Upload

---
+++ Servers/AD Attribute Change.ps1
@@ -0,0 +1,6 @@
+﻿Import-Csv -Path C:\KSMC\UserChangesTest.csv | Foreach-Object {
+    $user = $_.UserPrincipalName
+    $title = $_.Title
+    $department = $_.Department
+    Get-ADUser -Filter 'UserPrincipalName -eq $user' | Set-ADUser -Title $title -Department $department
+}
--- find-log4j.sh
+++
@@ -1,1 +0,0 @@
-sudo find / -name 'log4j*
